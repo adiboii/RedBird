0448a8c9d05188c7518b78ef59e4b9f8b9970062
adi.boyii <adireyes00@gmail.com>
2021-09-28 13:13:28 +0800

    views.py messages

--- RBApp/views.py
+++ RBApp/views.py
@@ -1,10 +1,11 @@
 from django.core.checks import messages
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import View
 from .forms import *
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.contrib import messages
 # Create your views here.
 
 class MyIndexView(View):
@@ -54,41 +55,47 @@ class UserFormView(View):
         
         def post(self, request):
                 form = UserForm(request.POST)
-            
-                if form.is_valid():
-                    fname = request.POST.get("first_name")
-                    lname = request.POST.get("last_name")
-                    uname = request.POST.get("username")
-                    pword = request.POST.get("password")
-                    pNumber = request.POST.get("phone_number")
-                    strt = request.POST.get("street")
-                    cty = request.POST.get("city")
-                    cntry = request.POST.get("country")
-                    form = User(username = uname, password = pword, first_name = fname, last_name = lname, phone_number = pNumber, street = strt, city = cty, country = cntry)
-                    form.save()
-                    
+                uname = request.POST.get("username")
+                pNumber = request.POST.get("phone_number")
+                fname = request.POST.get("first_name")
+                lname = request.POST.get("last_name")
+                pword = request.POST.get("password")
+                strt = request.POST.get("street")
+                cty = request.POST.get("city")
+                cntry = request.POST.get("country")
+                if User.objects.filter(username = uname).exists():
+                    messages.error(request, 'Username already exists')
                     return redirect('RBApp:my_dashboard_view')
                 else:
-                    messages.error(request, "Error")
-                    #return redirect('RBApp:my_dashboard_view')
+                    if str.isnumeric(pNumber):
+                        form = User(username = uname, password = pword, first_name = fname, last_name = lname, phone_number = pNumber, street = strt, city = cty, country = cntry)
+                        form.save()
+                        messages.success(request, "User successfully created.")
+                        return redirect('RBApp:my_dashboard_view')
+                    else:
+                        messages.error(request, 'Phone number must contain numbers only')
+                        return redirect('RBApp:my_dashboard_view')
                 
-
-
-
+                    
 
 class DishFormView(View):
     def get(self,request):
                 return render(request, 'forms/dish-form.html', {})
     def post(self, request):
                 form = DishForm(request.POST)
-                if form.is_valid():
-                    nme = request.POST.get("name")
-                    prce = request.POST.get("price")
-                    dscrptn = request.POST.get("description")
+                nme = request.POST.get("name")
+                prce = request.POST.get("price")
+                temp = prce.replace(".", "")
+                dscrptn = request.POST.get("description")
+                if temp.isnumeric():
                     form = Dish(name = nme, price = prce, description = dscrptn)
                     form.save()
-                    #return render(request, 'forms/user-form.html', {})
+                    messages.success(request, "Dish successfully created.")
                     return redirect('RBApp:my_dashboard_view')
+                else:
+                    messages.error(request, "Price must be positive numbers only")
+                    return redirect('RBApp:my_dashboard_view')
+
 
 
 class MenuTypeFormView(View):
@@ -102,4 +109,20 @@ class MenuTypeFormView(View):
                     form = MenuType(name = nme, description = dscrptn)
                     form.save()
                     #return render(request, 'Dashboard.html', {})
+                    messages.success(request, "Menu Type successfully created.")
                     return redirect('RBApp:my_dashboard_view')
+
+def delete_user(request, username):
+    obj = User.objects.get(username = username)
+    obj.delete()
+    return redirect('RBApp:my_dashboard_view')
+
+def delete_dish(request, dish_id):
+    dish = Dish.objects.get(dish_id = dish_id)
+    dish.delete()
+    return redirect('RBApp:my_dashboard_view')
+
+def delete_menu_type(request, type_id):
+    menu_type = MenuType.objects.get(type_id = type_id)
+    menu_type.delete()
+    return redirect('RBApp:my_dashboard_view')
